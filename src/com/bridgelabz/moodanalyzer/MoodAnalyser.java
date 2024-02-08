4a9d474be9556fe695b263c68a99486ed1d564be
mk3709 <mk3709@srmist.edu.in>
2024-02-08 16:11:45 +0530

    MoodAnalyser

---
+++ src/com/bridgelabz/moodanalyzer/MoodAnalyser.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.moodanalyzer;
+
+public class MoodAnalyser {
+    public static void main(String[] args) {
+
+           MoodAnalyser moodAnalyzer = new MoodAnalyser();
+
+
+
+
+    }
+
+}
